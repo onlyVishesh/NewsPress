7d49339c0cfd0799a1571f187d1363b2e2f0ba87
onlyVishesh <Visheshmaurya14@gmail.com>
2024-01-03 17:00:43 +0530

    final Commit

--- script.js
+++ script.js
@@ -2,36 +2,59 @@ const container = document.querySelector(".container");
 const optionsContainer = document.querySelector(".options-container");
 let country = "in"
 const countryList = [
+  { name: "United Arab Emirates", code: "AE" },
+  { name: "Argentina", code: "AR" },
+  { name: "Austria", code: "AT" },
   { name: "Australia", code: "AU" },
-  { name: "Bangladesh", code: "BD" },
   { name: "Belgium", code: "BE" },
-  { name: "Bhutan", code: "BT" },
+  { name: "Bulgaria", code: "BG" },
+  { name: "Brazil", code: "BR" },
+  { name: "Canada", code: "CA" },
+  { name: "Switzerland", code: "CH" },
   { name: "China", code: "CN" },
+  { name: "Colombia", code: "CO" },
+  { name: "Cuba", code: "CU" },
+  { name: "Czech Republic", code: "CZ" },
+  { name: "Germany", code: "DE" },
   { name: "Egypt", code: "EG" },
   { name: "France", code: "FR" },
-  { name: "Germany", code: "DE" },
+  { name: "United Kingdom", code: "GB" },
+  { name: "Greece", code: "GR" },
   { name: "Hong Kong", code: "HK" },
-  { name: "India", code: "IN" },
-  { name: "Iraq", code: "IQ" },
+  { name: "Hungary", code: "HU" },
+  { name: "Indonesia", code: "ID" },
   { name: "Ireland", code: "IE" },
+  { name: "Israel", code: "IL" },
+  { name: "India", code: "IN" },
+  { name: "Italy", code: "IT" },
   { name: "Japan", code: "JP" },
+  { name: "South Korea", code: "KR" },
+  { name: "Lithuania", code: "LT" },
+  { name: "Latvia", code: "LV" },
+  { name: "Morocco", code: "MA" },
   { name: "Mexico", code: "MX" },
-  { name: "Nepal", code: "NP" },
+  { name: "Malaysia", code: "MY" },
+  { name: "Nigeria", code: "NG" },
   { name: "Netherlands", code: "NL" },
   { name: "New Zealand", code: "NZ" },
-  { name: "Pakistan", code: "PK" },
-  { name: "Russian", code: "RU" },
+  { name: "Philippines", code: "PH" },
+  { name: "Poland", code: "PL" },
+  { name: "Portugal", code: "PT" },
+  { name: "Romania", code: "RO" },
+  { name: "Russia", code: "RU" },
   { name: "Saudi Arabia", code: "SA" },
-  { name: "Singapore", code: "SG" },
-  { name: "South Africa", code: "ZA" },
-  { name: "Sri Lanka", code: "LK" },
   { name: "Sweden", code: "SE" },
-  { name: "Switzerland", code: "CH" },
-  { name: "Taiwan", code: "TW" },
+  { name: "Singapore", code: "SG" },
+  { name: "Slovakia", code: "SK" },
+  { name: "Thailand", code: "TH" },
   { name: "Turkey", code: "TR" },
+  { name: "Taiwan", code: "TW" },
   { name: "Ukraine", code: "UA" },
-  { name: "United States of America", code: "US" },
+  { name: "United States", code: "US" },
+  { name: "Venezuela", code: "VE" },
+  { name: "South Africa", code: "ZA" },
 ];
+
 
 function createDropdown() {
   countryList.forEach((country) => {
@@ -111,11 +134,6 @@ document.querySelector(".search-btn").addEventListener("click", ()=> {
   selectCountry()
       
 })
-  
-
-// })
-
-
 
 //Options Buttons
 const createOptions = () => {
